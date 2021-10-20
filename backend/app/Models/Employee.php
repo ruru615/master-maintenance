597d4954b6543f6adda6da7a6f05ba0776cd7e36
ruru615 <92830739+ruru615@users.noreply.github.com>
2021-10-20 11:11:32 +0900

    Add files via upload

--- backend/app/Models/Employee.php
+++ backend/app/Models/Employee.php
@@ -1,1 +1,83 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Kyslik\ColumnSortable\Sortable;
+
+class Employee extends Model
+{
+    use HasFactory;
+    use Sortable;
+
+    protected $fillable = [
+        'empNumber',
+        'enterDay',
+        'id',
+        'lastName',
+        'firstName',
+        'lastKana',
+        'firsKana',
+        'realLast',
+        'realFirst',
+        'realLastKana',
+        'realFirstKana',
+        'birthDay',
+        'sexType',
+        'nationality',
+        'personalNumber',
+        'bloodType1',
+        'bloodType2',
+        'driversLicenseCategory',
+        'driversLicenseNumber',
+        'disabilityCertificate',
+        'residenceCardNumber',
+        'workVisa',
+        'assignName',
+        'positionCategory',
+        'positionStartDay',
+        'workplace1',
+        'workplace2',
+        'workplace3',
+        'companyMail',
+        'privateMail',
+        'companyMobile',
+        'privateMobile',
+        'privateTel',
+        'emergency1',
+        'emergency2',
+        'postalCode',
+        'address1',
+        'address2',
+        'address3',
+        'householder',
+        'educationCategory',
+        'finalEducation1',
+        'finalEducation2',
+        'finalEducation3',
+        'educationYear',
+        'hiringCategory',
+        'employmentCategory',
+        'salaryNumber',
+        'pensionNumber',
+        'insuranceNumber',
+        'leaveFrag',
+        'leaveDate',
+        'leaveMonths',
+        'experienceYears',
+        'exitDay',
+        'deleteFlag',
+        'insertDate',
+        'insertNumber',
+        'updateDate',
+        'updateNumber'
+    ];
+
+    public $sortable = [
+        'empNumber',
+        'id',
+        'lastName'
+    ];
+}
 
